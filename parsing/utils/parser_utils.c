5dd9d68d3918bb2d262e2dc704d49b68480822e2
bofar26 <bofar26@gmail.com>
2025-12-09 18:53:12 +0100

    feat: add dispatch logic and parser base framework

--- parsing/utils/parser_utils.c
+++ parsing/utils/parser_utils.c
@@ -6,20 +6,45 @@
 /*   By: mipang <mipang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 19:11:10 by mipang            #+#    #+#             */
-/*   Updated: 2025/12/03 16:47:58 by mipang           ###   ########.fr       */
+/*   Updated: 2025/12/09 18:14:35 by mipang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void    parser_error(int lineidx, const char *msg)
+void	parser_error(int lineidx, const char *msg)
 {
-    ft_printf("Error"\n);
-    ft_printf("line no.%d : %s", lineidx, msg);
-    exit(EXIT_FAILURE);
+	ft_printf("Error"\n);
+	ft_printf("line no.%d : %s", lineidx, msg);
+	exit(EXIT_FAILURE);
 }
 
-t_scene scene_init(t_scene sc)
+void	trim_space(char	*str)
 {
+	char	*start;
+	char	*end;
 
+	if (!str)
+		return ;
+	start = str;
+	end = start + ft_strlen(str);
+	while (*start && ((*start == ' ') || *start == '\t'))
+		start ++;
+	while (end > start && ((end[-1] == ' ') || (end[-1] == '\t')
+		|| (end[-1] == '\n') || (end[-1] == '\r')))
+		end --;
+	*end = '\0';
+	if (start != str)
+		ft_memmove(str, start, end - start + 1);
+}
+
+t_scene	*scene_init(void)
+{
+	t_scene *sc;
+
+	sc = malloc(sizeof(t_scene));
+	if (!sc)
+		return (NULL);
+	ft_memset(sc, 0, sizeof(t_scene));
+	return (sc);
 }
